3dbe996af79939e126e9d4b8f533ce2f4da79945
xiejinjie <544482194@qq.com>
2023-03-05 16:08:59 +0800

    调整初始位置

--- src/main/java/top/kcoder/wordbook/view/AppFrameStyle.java
+++ src/main/java/top/kcoder/wordbook/view/AppFrameStyle.java
@@ -15,7 +15,7 @@ public class AppFrameStyle {
     public static final int FRAME_WIDTH = 600;
     public static final int FRAME_HEIGHT = 600;
     public static final int FRAME_LOCATION_X = 300;
-    public static final int FRAME_LOCATION_Y = 150;
+    public static final int FRAME_LOCATION_Y = 100;
     public static final Color FRAME_BACKGROUND_COLOR = new Color(77, 77, 77);
 
     public static final Font GLOBAL_FONT = new Font("微软雅黑", Font.BOLD, 18);
